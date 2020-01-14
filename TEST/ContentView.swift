b73f5cc6cefa2d4d48ecbd618126c800928801ac
c8610290927 <c8610290927@gmail.com>
2020-01-14 17:23:29 +0800

    Initial Commit

---
+++ TEST/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TEST
+//
+//  Created by User20 on 2020/1/14.
+//  Copyright © 2020 polon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
